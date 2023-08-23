a1cee7005e9bd48cc7f080329a6179c27e1e8232
chiemezie1 <chiemezieagbo1@gmail.com>
2023-08-23 15:15:09 +0100

    svg

---
+++ horizonclient/src/svg/index.js
@@ -0,0 +1,47 @@
+import add from './add.svg'
+import menu from './menu.svg'
+import logout from './logout.svg'
+import more_vert from './more_vert.svg'
+import toggle_off from './toggle_off.svg'
+import thumb_up from './thumb_up.svg'
+import share from './share.svg'
+import group from './group.svg'
+import groups from './groups.svg'
+import person_add from './person_add.svg'
+import person from './person.svg'
+import visibility from './visibility.svg'
+import mail from './mail.svg'
+import send from './send.svg'
+import upload_file from './upload_file.svg'
+import person_search from './person_search.svg'
+import mode_comment from './mode_comment.svg'
+import phone_enabled from './phone_enabled.svg'
+import photo_camera from './photo_camera.svg'
+import edit from './edit.svg'
+
+
+
+
+
+export{
+    add,
+    menu,
+    logout,
+    more_vert,
+    toggle_off,
+    thumb_up,
+    share,
+    group,
+    groups,
+    person_add,
+    person,
+    visibility,
+    mail,
+    send,
+    upload_file,
+    person_search,
+    mode_comment,
+    phone_enabled,
+    photo_camera,
+    edit,
+}
