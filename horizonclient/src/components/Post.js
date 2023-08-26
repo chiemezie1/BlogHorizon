52940fe8d1ae7910650c71c7cf6590d02ff3dd4f
chiemezie1 <chiemezieagbo1@gmail.com>
2023-08-26 22:50:20 +0100

    user profile

--- horizonclient/src/components/Post.js
+++ horizonclient/src/components/Post.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ProfileImage from "../components/ProfileImage";
+import { share, mode_comment } from "../svg/index";
 
 function Post() {
   return (
     <div>
-      <ProfileImage svg={svg} size_w={6} size_h={6} />
+      <div className='border-2 border-gray-100 shadow-md'>
+        <dv className='p-2 justify-start '>
+        <ProfileImage svg={svg} size_w={6} size_h={6} />
+        </dv>
+
+      </div>
       
     </div>
   )
