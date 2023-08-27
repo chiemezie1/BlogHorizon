589ec021d8124d950880949a82848a3b9c8d8729
chiemezie1 <chiemezieagbo1@gmail.com>
2023-08-27 20:51:38 +0100

    update

--- horizonclient/src/components/Post.js
+++ horizonclient/src/components/Post.js
@@ -1,19 +1,21 @@
-import React from 'react';
+import React from "react";
 import ProfileImage from "../components/ProfileImage";
 import { share, mode_comment } from "../svg/index";
 
-function Post({svg, size_h, size_w}) {
+function Post({ svg, size_h, size_w, name, content, likes, comment }) {
   return (
-    <div>
-      <div className='border-2 border-gray-100 shadow-md'>
-        <dv className='p-2 justify-start '>
-        <ProfileImage svg={svg} size_w={6} size_h={6} />
-        </dv>
-
+    <div className="p-8">
+      <div className="border-2 border-gray-100 shadow-md">
+        <div className="p-2 flex gap-1 justify-start ">
+          <ProfileImage svg={svg} size_w={size_w} size_h={size_h} />
+          <div className="font-semibold">{name}</div>
+        </div>
+        <div>
+          {content}
+        </div>
       </div>
-      
     </div>
-  )
+  );
 }
 
-export default Post
+export default Post;
--- horizonclient/src/pages/PostDetail.js
+++ horizonclient/src/pages/PostDetail.js
@@ -2,9 +2,14 @@ import React from "react";
 import Post from "../components/Post";
 
 function PostDetail() {
+  const svg = "svg/path";
+  const name = "chiemezie Agbo";
+  const likes = ["chiemezie", "sunday", "love", "kindness"];
+  const comment = ["woow", "goood", "love"];
+
   return (
     <div>
-      <Post />
+      <Post svg={svg} size_h={6} size_w={6} name={name} likes={likes} comment={comment} />
     </div>
   );
 }
