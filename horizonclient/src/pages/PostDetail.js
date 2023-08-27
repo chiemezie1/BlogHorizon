bd060cfa44b1d2925b7973bc022ec5c5f55ef45e
chiemezie1 <chiemezieagbo1@gmail.com>
2023-08-27 20:33:06 +0100

    update

--- horizonclient/src/pages/PostDetail.js
+++ horizonclient/src/pages/PostDetail.js
@@ -4,9 +4,7 @@ import Post from "../components/Post";
 function PostDetail() {
   return (
     <div>
-      <div>i want to see if this is working </div>
       <Post />
-      <div>hello</div>
     </div>
   );
 }
