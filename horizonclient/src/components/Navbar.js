0c76e3170073454f212fb0b819170d52e8613f65
chiemezie1 <chiemezieagbo1@gmail.com>
2023-09-10 19:58:22 +0100

    navbar

--- horizonclient/src/components/Navbar.js
+++ horizonclient/src/components/Navbar.js
@@ -44,7 +44,7 @@ import SearchBar from "../components/SearchBar";
 
 const Navbar = () => {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
-    const [showNav, setShowNav] = useState(false); // New state to control the visibility of the navigation
+    const [showNav, setShowNav] = useState(false);
 
     const handleSearch = (query) => {
         console.log("Searching for:", query);
@@ -54,20 +54,38 @@ const Navbar = () => {
         <nav className="bg-white shadow-lg m-2">
             <div className="container mx-auto px-4 py-4 flex justify-between items-center">
                 {/* Logo on the Left */}
-                <div>
+                <div className="flex-grow">
                     <h1 className="text-xl font-bold text-gray-700">Logo</h1>
                 </div>
 
-                {/* Search Bar in the Center */}
-                <SearchBar onSearch={handleSearch} />
+                {/* Search Bar */}
+                <div className="hidden md:block">
+                    <SearchBar onSearch={handleSearch} />
+                </div>
 
-                {/* Hamburger on the Right */}
-                <button onClick={() => setShowNav(!showNav)} className="text-gray-600 hover:text-gray-900">
-                    ☰
-                </button>
+                {/* Navigation for large screens */}
+                <div className="hidden md:flex items-center space-x-4">
+                    {isLoggedIn ? (
+                        <>
+                            <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Create Post</button>
+                            <button onClick={() => setIsLoggedIn(false)} className="text-gray-700 hover:text-gray-900">Logout</button>
+                            <span className="text-gray-700 hover:text-gray-900 cursor-pointer">Profile 🚹</span>
+                        </>
+                    ) : (
+                        <>
+                            <button className="text-gray-700 hover:text-gray-900">Register</button>
+                            <button onClick={() => setIsLoggedIn(true)} className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Login</button>
+                        </>
+                    )}
+                </div>
 
-                {/* Dropdown menu */}
-                <div className={`fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 ${showNav ? 'block' : 'hidden'}`} onClick={() => setShowNav(false)}>
+                {/* Hamburger and Navigation for small screens */}
+                <div className="md:hidden flex items-center">
+                    <button onClick={() => setShowNav(!showNav)} className="text-gray-600 hover:text-gray-900">
+                        ☰
+                    </button>
+                </div>
+                <div className={`fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 ${showNav ? 'block' : 'hidden'} md:hidden`} onClick={() => setShowNav(false)}>
                     <div className="bg-white w-64 h-full fixed top-0 right-0 overflow-y-auto py-4">
                         {isLoggedIn ? (
                             <div className="flex flex-col space-y-4">
