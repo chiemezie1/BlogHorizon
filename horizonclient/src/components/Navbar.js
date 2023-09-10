47dcf045a48963678d555365605b6d342de2bb14
chiemezie1 <chiemezieagbo1@gmail.com>
2023-09-10 19:48:41 +0100

    navbar

--- horizonclient/src/components/Navbar.js
+++ horizonclient/src/components/Navbar.js
@@ -1,8 +1,50 @@
+// import React, { useState } from 'react';
+// import SearchBar from "../components/SearchBar";
+
+// const Navbar = () => {
+//     const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+//     const handleSearch = (query) => {
+//         console.log("Searching for:", query);
+//     };
+
+//     return (
+//         <nav className="bg-white shadow-lg m-2">
+//             <div className="container mx-auto px-4 py-4 sm:flex sm:justify-between sm:items-center">
+//                 {/* Logo on the Left */}
+//                 <div className="mb-4 sm:mb-0">
+//                     <h1 className="text-xl font-bold text-gray-700">Logo</h1>
+//                 </div>
+//                 <SearchBar onSearch={handleSearch} />
+//                 {/* Depending on login status, show appropriate links */}
+//                 {isLoggedIn ? (
+//                     <div className="flex space-x-2 sm:space-x-4 items-center mt-4 sm:mt-0">
+//                         <button className="bg-blue-500 text-white px-3 sm:px-4 py-2 rounded hover:bg-blue-600">Create Post</button>
+//                         <button onClick={() => setIsLoggedIn(false)} className="text-gray-700 hover:text-gray-900">Logout</button>
+//                         <span className="text-gray-700 hover:text-gray-900 cursor-pointer">
+//                             Profile 🚹
+//                         </span>
+//                     </div>
+//                 ) : (
+//                     <div className="flex space-x-2 sm:space-x-4 mt-4 sm:mt-0">
+//                         <button className="text-gray-700 hover:text-gray-900">Register</button>
+//                         <button onClick={() => setIsLoggedIn(true)} className="bg-blue-500 text-white px-3 sm:px-4 py-2 rounded hover:bg-blue-600">Login</button>
+//                     </div>
+//                 )}
+//             </div>
+//         </nav>
+//     );
+// }
+
+// export default Navbar;
+
+
 import React, { useState } from 'react';
 import SearchBar from "../components/SearchBar";
 
 const Navbar = () => {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [showNav, setShowNav] = useState(false); // New state to control the visibility of the navigation
 
     const handleSearch = (query) => {
         console.log("Searching for:", query);
@@ -10,27 +52,39 @@ const Navbar = () => {
 
     return (
         <nav className="bg-white shadow-lg m-2">
-            <div className="container mx-auto px-4 py-4 sm:flex sm:justify-between sm:items-center">
+            <div className="container mx-auto px-4 py-4 flex justify-between items-center">
                 {/* Logo on the Left */}
-                <div className="mb-4 sm:mb-0">
+                <div>
                     <h1 className="text-xl font-bold text-gray-700">Logo</h1>
                 </div>
+
+                {/* Search Bar in the Center */}
                 <SearchBar onSearch={handleSearch} />
-                {/* Depending on login status, show appropriate links */}
-                {isLoggedIn ? (
-                    <div className="flex space-x-2 sm:space-x-4 items-center mt-4 sm:mt-0">
-                        <button className="bg-blue-500 text-white px-3 sm:px-4 py-2 rounded hover:bg-blue-600">Create Post</button>
-                        <button onClick={() => setIsLoggedIn(false)} className="text-gray-700 hover:text-gray-900">Logout</button>
-                        <span className="text-gray-700 hover:text-gray-900 cursor-pointer">
-                            Profile 🚹
-                        </span>
+
+                {/* Hamburger on the Right */}
+                <button onClick={() => setShowNav(!showNav)} className="text-gray-600 hover:text-gray-900">
+                    ☰
+                </button>
+
+                {/* Dropdown menu */}
+                <div className={`fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 ${showNav ? 'block' : 'hidden'}`} onClick={() => setShowNav(false)}>
+                    <div className="bg-white w-64 h-full fixed top-0 right-0 overflow-y-auto py-4">
+                        {isLoggedIn ? (
+                            <div className="flex flex-col space-y-4">
+                                <button onClick={() => setIsLoggedIn(false)} className="text-gray-700 hover:text-gray-900 px-4 py-2">Logout</button>
+                                <span className="text-gray-700 hover:text-gray-900 cursor-pointer px-4 py-2">
+                                    Profile 🚹
+                                </span>
+                                <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Create Post</button>
+                            </div>
+                        ) : (
+                            <div className="flex flex-col space-y-4">
+                                <button className="text-gray-700 hover:text-gray-900 px-4 py-2">Register</button>
+                                <button onClick={() => setIsLoggedIn(true)} className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Login</button>
+                            </div>
+                        )}
                     </div>
-                ) : (
-                    <div className="flex space-x-2 sm:space-x-4 mt-4 sm:mt-0">
-                        <button className="text-gray-700 hover:text-gray-900">Register</button>
-                        <button onClick={() => setIsLoggedIn(true)} className="bg-blue-500 text-white px-3 sm:px-4 py-2 rounded hover:bg-blue-600">Login</button>
-                    </div>
-                )}
+                </div>
             </div>
         </nav>
     );
--- horizonclient/src/components/SearchBar.js
+++ horizonclient/src/components/SearchBar.js
@@ -1,47 +1,81 @@
+import {search} from "../svg/index"
 import React, { useState } from 'react';
-import SearchBar from "../components/SearchBar";
 
-const Navbar = () => {
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-    const [showNav, setShowNav] = useState(false); // New state to control the visibility of the navigation
+const SearchBar = ({ onSearch }) => {
+  const [searchTerm, setSearchTerm] = useState('');
 
-    const handleSearch = (query) => {
-        console.log("Searching for:", query);
-    };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSearch(searchTerm);
+  };
 
-    return (
-        <nav className="bg-white shadow-lg m-2">
-            <div className="container mx-auto px-4 py-4 sm:flex sm:justify-between sm:items-center">
-                {/* Logo on the Left */}
-                <div className="flex justify-between items-center">
-                    <h1 className="text-xl font-bold text-gray-700">Logo</h1>
-                    <button onClick={() => setShowNav(!showNav)} className="sm:hidden text-gray-600 hover:text-gray-900">
-                        ☰
-                    </button>
-                </div>
+  return (
+    <div className="bg-white rounded max-w-sm mx-auto border-2 border-gray-300">
+      <form onSubmit={handleSubmit} className="flex items-center">
+        <input 
+          type="text" 
+          className="flex-grow p-2 rounded-l-md focus:outline-none focus:border-gray-500 text-sm"
+          placeholder="Search..."
+          value={searchTerm} 
+          onChange={(e) => setSearchTerm(e.target.value)} 
+        />
+        <button type="submit">
+        <img src={search} alt="share" className="h-10 w-10 p-2" />
+        </button>
+      </form>
+    </div>
+  );
+};
 
-                <SearchBar onSearch={handleSearch} />
+export default SearchBar;
 
-                {/* Main Navigation */}
-                <div className={`mt-4 sm:mt-0 ${showNav ? 'block' : 'hidden'} sm:flex`}>
-                    {isLoggedIn ? (
-                        <div className="flex space-x-4 items-center">
-                            <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Create Post</button>
-                            <button onClick={() => setIsLoggedIn(false)} className="text-gray-700 hover:text-gray-900">Logout</button>
-                            <span className="text-gray-700 hover:text-gray-900 cursor-pointer">
-                                Profile 🚹
-                            </span>
-                        </div>
-                    ) : (
-                        <div className="flex space-x-4">
-                            <button className="text-gray-700 hover:text-gray-900">Register</button>
-                            <button onClick={() => setIsLoggedIn(true)} className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Login</button>
-                        </div>
-                    )}
-                </div>
-            </div>
-        </nav>
-    );
-}
 
-export default Navbar;
+
+
+// import React, { useState } from 'react';
+// import SearchBar from "../components/SearchBar";
+
+// const Navbar = () => {
+//     const [isLoggedIn, setIsLoggedIn] = useState(false);
+//     const [showNav, setShowNav] = useState(false); // New state to control the visibility of the navigation
+
+//     const handleSearch = (query) => {
+//         console.log("Searching for:", query);
+//     };
+
+//     return (
+//         <nav className="bg-white shadow-lg m-2">
+//             <div className="container mx-auto px-4 py-4 sm:flex sm:justify-between sm:items-center">
+//                 {/* Logo on the Left */}
+//                 <div className="flex justify-between items-center">
+//                     <h1 className="text-xl font-bold text-gray-700">Logo</h1>
+//                     <button onClick={() => setShowNav(!showNav)} className="sm:hidden text-gray-600 hover:text-gray-900">
+//                         ☰
+//                     </button>
+//                 </div>
+
+//                 <SearchBar onSearch={handleSearch} />
+
+//                 {/* Main Navigation */}
+//                 <div className={`mt-4 sm:mt-0 ${showNav ? 'block' : 'hidden'} sm:flex`}>
+//                     {isLoggedIn ? (
+//                         <div className="flex space-x-4 items-center">
+//                             <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Create Post</button>
+//                             <button onClick={() => setIsLoggedIn(false)} className="text-gray-700 hover:text-gray-900">Logout</button>
+//                             <span className="text-gray-700 hover:text-gray-900 cursor-pointer">
+//                                 Profile 🚹
+//                             </span>
+//                         </div>
+//                     ) : (
+//                         <div className="flex space-x-4">
+//                             <button className="text-gray-700 hover:text-gray-900">Register</button>
+//                             <button onClick={() => setIsLoggedIn(true)} className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Login</button>
+//                         </div>
+//                     )}
+//                 </div>
+//             </div>
+//         </nav>
+//     );
+// }
+
+// export default Navbar;
