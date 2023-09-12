ae155a95aa20dab129811d2a9cacfa0cfeb0a299
chiemezie1 <chiemezieagbo1@gmail.com>
2023-09-12 21:24:38 +0100

    comment route

--- backend/routes/commentRoutes.js
+++ backend/routes/commentRoutes.js
@@ -1,106 +1,28 @@
 const express = require('express');
+const CommentController = require('../controllers/CommentController');
+const ensureAuthenticated = require('../middleware/auth');
+
 const router = express.Router();
-const ensureAuthenticated  = require('../middleware/auth');
-const Comment = require('../models/commentModel');
 
-// List all comments for a post
-router.get('/posts/:postId/comments', async (req, res) => {
-    try {
-        const comments = await Comment.find({ postId: req.params.postId });
-        res.json(comments);
-    } catch (error) {
-        res.status(500).json({ message: 'Server Error' });
-    }
-});
+// Route to create a new comment for a post
+router.post('/', ensureAuthenticated, CommentController.createComment);
 
-// Add a comment to a post
-router.post('/posts/:postId/comments', ensureAuthenticated, async (req, res) => {
-    try {
-        const newComment = new Comment({
-            postId: req.params.postId,
-            userId: req.user.id,
-            text: req.body.text,
-        });
+// Route to get all comments for a specific post
+router.get('/post/:postId', CommentController.getCommentsByPost);
 
-        const comment = await newComment.save();
-        res.json(comment);
-    } catch (error) {
-        res.status(500).json({ message: 'Server Error' });
-    }
-});
 
-// Update a comment
-router.put('/comments/:commentId', ensureAuthenticated, async (req, res) => {
-    try {
-        let comment = await Comment.findById(req.params.commentId);
-        if (!comment) return res.status(404).json({ message: 'Comment not found' });
+// Private routes
 
-        // Ensure the user is the owner of the comment
-        if (comment.userId.toString() !== req.user.id) {
-            return res.status(401).json({ message: 'Unauthorized' });
-        }
+// Route to update a comment
+router.put('/:commentId', ensureAuthenticated, CommentController.updateComment);
 
-        comment = await Comment.findByIdAndUpdate(req.params.commentId, req.body, { new: true });
-        res.json(comment);
-    } catch (error) {
-        res.status(500).json({ message: 'Server Error' });
-    }
-});
+// Route to delete a comment
+router.delete('/:commentId', ensureAuthenticated, CommentController.deleteComment);
 
-// Delete a comment
-router.delete('/comments/:commentId', ensureAuthenticated, async (req, res) => {
-    try {
-        const comment = await Comment.findById(req.params.commentId);
-        if (!comment) return res.status(404).json({ message: 'Comment not found' });
+// Route to like a comment
+router.post('/:commentId/like', ensureAuthenticated, CommentController.likeComment);
 
-        // Ensure the user is the owner of the comment
-        if (comment.userId.toString() !== req.user.id) {
-            return res.status(401).json({ message: 'Unauthorized' });
-        }
-
-        await comment.remove();
-        res.json({ message: 'Comment removed' });
-    } catch (error) {
-        res.status(500).json({ message: 'Server Error' });
-    }
-});
-
-// Like a comment
-router.post('/comments/:commentId/like', ensureAuthenticated, async (req, res) => {
-    try {
-        const comment = await Comment.findById(req.params.commentId);
-        if (!comment) return res.status(404).json({ message: 'Comment not found' });
-
-        // Check if the user has already liked the comment
-        if (comment.likes.includes(req.user.id)) {
-            return res.status(400).json({ message: 'You already liked this comment' });
-        }
-
-        comment.likes.push(req.user.id);
-        await comment.save();
-        res.json(comment);
-    } catch (error) {
-        res.status(500).json({ message: 'Server Error' });
-    }
-});
-
-// Unlike a comment
-router.post('/comments/:commentId/unlike', ensureAuthenticated, async (req, res) => {
-    try {
-        const comment = await Comment.findById(req.params.commentId);
-        if (!comment) return res.status(404).json({ message: 'Comment not found' });
-
-        const index = comment.likes.indexOf(req.user.id);
-        if (index === -1) {
-            return res.status(400).json({ message: 'You have not liked this comment yet' });
-        }
-
-        comment.likes.splice(index, 1);
-        await comment.save();
-        res.json(comment);
-    } catch (error) {
-        res.status(500).json({ message: 'Server Error' });
-    }
-});
+// Route to unlike a comment
+router.post('/:commentId/unlike', ensureAuthenticated, CommentController.unlikeComment);
 
 module.exports = router;
