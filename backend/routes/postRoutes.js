033a142a493d438f6797fd09db87934a3acc2caf
chiemezie1 <chiemezieagbo1@gmail.com>
2023-09-12 21:09:43 +0100

    Post route

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,65 +1,22 @@
 const express = require('express');
 const router = express.Router();
-const Post = require('../models/postModel');
 
-// CREATE a new post
-router.post('/', async (req, res) => {
-    try {
-        const post = new Post(req.body);
-        await post.save();
-        res.status(201).send(post);
-    } catch (error) {
-        res.status(400).send(error);
-    }
-});
+const PostController = require('../controllers/PostController');
+const ensureAuthenticated = require('../middleware/auth');
 
-// GET all posts
-router.get('/', async (req, res) => {
-    try {
-        const posts = await Post.find();
-        res.status(200).send(posts);
-    } catch (error) {
-        res.status(500).send(error);
-    }
-});
+// Create a new post
+router.post('/create', ensureAuthenticated, PostController.createPost);
 
-// GET a single post by ID
-router.get('/:id', async (req, res) => {
-    try {
-        const post = await Post.findById(req.params.id);
-        if (!post) {
-            return res.status(404).send();
-        }
-        res.status(200).send(post);
-    } catch (error) {
-        res.status(500).send(error);
-    }
-});
+// Get all posts
+router.get('/', PostController.getAllPosts);
 
-// UPDATE a post by ID
-router.patch('/:id', async (req, res) => {
-    try {
-        const post = await Post.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
-        if (!post) {
-            return res.status(404).send();
-        }
-        res.status(200).send(post);
-    } catch (error) {
-        res.status(400).send(error);
-    }
-});
+// Get a specific post by ID
+router.get('/:postId', PostController.getPostById);
 
-// DELETE a post by ID
-router.delete('/:id', async (req, res) => {
-    try {
-        const post = await Post.findByIdAndDelete(req.params.id);
-        if (!post) {
-            return res.status(404).send();
-        }
-        res.status(200).send(post);
-    } catch (error) {
-        res.status(500).send(error);
-    }
-});
+// Update a post by ID
+router.put('/:postId', ensureAuthenticated, PostController.updatePost);
+
+// Delete a post by ID
+router.delete('/:postId', ensureAuthenticated, PostController.deletePost);
 
 module.exports = router;
