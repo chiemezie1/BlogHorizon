dcf0885d68360192c30349d5af8e6694e31362d5
chiemezie1 <chiemezieagbo1@gmail.com>
2023-09-15 21:20:46 +0100

    updated the profile data

--- horizonclient/src/pages/UserProfile.js
+++ horizonclient/src/pages/UserProfile.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import ProfileImage from "../components/ProfileImage";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
@@ -6,17 +6,43 @@ import { Link } from "react-router-dom";
 import { post, schedule, x_png, settings, home, phone_msg } from "../svg/index";
 
 function UserProfile() {
+  const [profile, setProfile] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+
+  useEffect(() => {
+    fetch('http://localhost:4000/getProfile', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    })
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then((data) => {
+            setProfile(data);
+            setLoading(false);
+        })
+        .catch((error) => {
+            setError(error);
+            setLoading(false);
+        });
+}, []);
+
+if (loading) {
+    return <div>Loading...</div>;
+}
+
+if (error) {
+    return <div>Error: {error.message}</div>;
+}
+
   const svg = "path/to/your/svg";
-  const name = "Chiemezie Agbo";
-  const [LivesIn, Joined, Posts, Following, Followers, Whatsapp, x] = [
-    "Enugu",
-    "June 2023",
-    "9",
-    "335",
-    "366",
-    "+2340816545430",
-    "@chiemezieagbo",
-  ];
 
   return (
     <>
@@ -33,18 +59,17 @@ function UserProfile() {
             <ProfileImage svg={svg} size_w={64} size_h={64} />
             <div className="md:pl-4">
               <div className="text-black text-2xl font-bold text-center md:text-start leading-8">
-                {name}
+                {profile.firstname+ " "+profile.lastname }
               </div>
               <div className="max-w-sm text-center md:text-start pt-2 leading-5">
-                Hyy i'm a blockchain developer, currently working with
-                BlogHorizon
+              {profile.introduction}
               </div>
               <div className="flex flex-row justify-evenly md:flex-col pt-4 ">
                 <div className="font-semibold text-gray-700">
-                  Following: <span className="font-bold">{Following} </span>
+                  Following: <span className="font-bold">{profile.following.length}</span>
                 </div>
                 <div className="font-semibold text-gray-700">
-                  Followers: <span className="font-bold">{Followers} </span>
+                  Followers: <span className="font-bold">{profile.followers.length}</span>
                 </div>
               </div>
             </div>
@@ -53,31 +78,31 @@ function UserProfile() {
             <div className=" flex p-2 gap-1 items-center">
               <img src={post} alt="-" className="h-4 w-4" />
               <div className="font-semibold text-gray-700">
-                Posts: <span className="font-bold">{Posts} </span>
+                Posts: <span className="font-bold">{profile.posts.length}</span>
               </div>
             </div>
             <div className=" flex p-2 gap-1 items-center">
               <img src={schedule} alt="-" className="h-4 w-4" />
               <div className="font-semibold text-gray-700">
-                Joined: <span className="font-bold">{Joined} </span>
+                Joined: <span className="font-bold">{new Date(profile.joined).toLocaleDateString()}</span>
               </div>
             </div>
             <div className=" flex p-2 gap-1 items-center">
               <img src={home} alt="-" className="h-4 w-4" />
               <div className="font-semibold text-gray-700">
-                Lives in: <span className="font-bold">{LivesIn} </span>
+                Lives in: <span className="font-bold">{profile.location || " "}</span>
               </div>
             </div>
             <div className=" flex p-2 gap-1 items-center">
               <img src={phone_msg} alt="-" className="h-4 w-4" />
               <div className="font-semibold text-gray-700">
-                WhatsApp: <span className="font-bold">{Whatsapp} </span>
+                WhatsApp: <span className="font-bold">{profile.whatsapp}</span>
               </div>
             </div>
             <div className=" flex p-2 gap-1 items-center">
               <img src={x_png} alt="-" className="h-4 w-4" />
               <div className="font-semibold text-gray-700">
-                : <span className="font-bold">{x} </span>
+                : <span className="font-bold">{profile.twitterHandle}</span>
               </div>
             </div>
           </div>
