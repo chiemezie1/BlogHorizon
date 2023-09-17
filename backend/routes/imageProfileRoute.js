8b18c62474e6f4536dd0eb07c3c2a9658f15d981
chiemezie1 <chiemezieagbo1@gmail.com>
2023-09-17 20:55:38 +0100

    update

--- backend/routes/imageProfileRoute.js
+++ backend/routes/imageProfileRoute.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads/profile-images/') 
+    cb(null, '../images/post/') 
   },
   filename: (req, file, cb) => {
     cb(null, Date.now() + '-' + file.originalname)
